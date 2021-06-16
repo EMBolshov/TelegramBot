d2b28f2cbce734483d93003e56bda4988a50ac99
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-06-16 23:45:25 +0300

    Remove debug logging

--- TelegramBot.Api/Models/Commands/EchoCommand.cs
+++ TelegramBot.Api/Models/Commands/EchoCommand.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
+﻿using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -8,34 +6,13 @@ namespace TelegramBot.Api.Models.Commands
 {
     public class EchoCommand : ICommand
     {
-        private readonly ILogger<EchoCommand> _logger;
-
-        public EchoCommand()
-        {
-            
-        }
-        
-        public EchoCommand(ILogger<EchoCommand> logger)
-        {
-            _logger = logger;
-        }
-
         public string Name => "/echo";
         
         public async Task Execute(Message message, TelegramBotClient client)
         {
             var chatId = message.Chat.Id;
             var messageId = message.MessageId;
-
-            _logger.LogWarning($"ChatId: {chatId}, MessageId: {messageId}");
-            try
-            {
-                await client.SendTextMessageAsync(chatId, $"Echo: {message.Text}", replyToMessageId: messageId);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.ToString());
-            }
+            await client.SendTextMessageAsync(chatId, $"Echo: {message.Text}", replyToMessageId: messageId);
         }
     }
 }
